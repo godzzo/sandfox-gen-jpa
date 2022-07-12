22f712ce4fa346baa10de2f630bae89060bd315d
EGodzzo <egodzzo@gmail.com>
2022-07-12 08:01:17 +0200

    upgrade SB versions

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
 ts-node --project src/tsconfig.json src/main.ts --config config/widgetz.json save
+
+ts-node --project src/tsconfig.json src/main.ts --config config/simple-nest.json save
+ts-node --project src/tsconfig.json src/main.ts --config config/simple-nest.json generate
+ts-node --project src/tsconfig.json src/main.ts --config config/simple-nest.json custom
